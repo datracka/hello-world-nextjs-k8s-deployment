268d74fc4257bd7e4eb6bbc67dd6e134f251bf53
Vicens Fayos <datracka@gmail.com>
2024-12-25 09:48:22 +0100

    add temporally the sensitive data to show'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error("Database connection error:", error.message);
-      dbMessage = `Failed to connect to the database: ${error.message}`;
+      dbMessage = `Failed to connect to the database: ${error.message} ${process.env.DATABASE_HOST} ${process.env.DATABASE_PORT} ${process.env.DATABASE_USER} ${process.env.DATABASE_PASSWORD} ${process.env.DATABASE_NAME}`;
     } else {
       console.error("Unexpected error:", error);
       dbMessage = "Failed to connect to the database: An unexpected error occurred.";
