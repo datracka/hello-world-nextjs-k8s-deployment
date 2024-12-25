c2de826f9535305d215a67dd9aedb2df6c30f021
Vicens Fayos <datracka@gmail.com>
2024-12-25 20:27:03 +0100

    simplify replacement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,17 @@ import getConfig from "next/config";
 
 const { serverRuntimeConfig } = getConfig();
 
+console.log("Environment Variables:", {
+  test: serverRuntimeConfig.TEST_ENV,
+  host: serverRuntimeConfig.DATABASE_HOST,
+  port: Number(serverRuntimeConfig.DATABASE_PORT),
+  user: serverRuntimeConfig.DATABASE_USER,
+  password: serverRuntimeConfig.DATABASE_PASSWORD,
+  database: serverRuntimeConfig.DATABASE_NAME,
+});
 
 export default async function Home() {
   let dbMessage = "Loading database connection status...";
-
-  console.log("Environment Variables:", {
-    host: serverRuntimeConfig.DATABASE_HOST,
-    port: Number(serverRuntimeConfig.DATABASE_PORT),
-    user: serverRuntimeConfig.DATABASE_USER,
-    password: serverRuntimeConfig.DATABASE_PASSWORD,
-    database: serverRuntimeConfig.DATABASE_NAME,
-  });
 
   try {
     const pool = new Pool({
@@ -33,7 +33,7 @@ export default async function Home() {
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error("Database connection error:", error.message);
-      dbMessage = `Failed to connect to the database: ${error.message} ${process.env.DATABASE_HOST} ${process.env.DATABASE_PORT} ${process.env.DATABASE_USER} ${process.env.DATABASE_PASSWORD} ${process.env.DATABASE_NAME}`;
+      dbMessage = `Failed to connect to the database: ${error.message} ${serverRuntimeConfig.TEST_ENV} ${serverRuntimeConfig.DATABASE_HOST} ${serverRuntimeConfig.DATABASE_PORT} ${serverRuntimeConfig.DATABASE_USER} ${serverRuntimeConfig.DATABASE_PASSWORD} ${serverRuntimeConfig.DATABASE_NAME}`;
     } else {
       console.error("Unexpected error:", error);
       dbMessage = "Failed to connect to the database: An unexpected error occurred.";
@@ -43,7 +43,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-lg font-bold">Database Connection Test 3</h1>
+        <h1 className="text-lg font-bold">Database Connection Test 4</h1>
         <p>{dbMessage}</p>
       </main>
     </div>
