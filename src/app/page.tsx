48afba58359acfdc8a5530eacb75be807030fdcb
Vicens Fayos <datracka@gmail.com>
2024-12-25 12:19:46 +0100

    force rebuild

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-lg font-bold">Database Connection Test 2</h1>
+        <h1 className="text-lg font-bold">Database Connection Test 3</h1>
         <p>{dbMessage}</p>
       </main>
     </div>
