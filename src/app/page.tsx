b1223e702fb8b4ad8242f09294a6113bad623760
Vicens Fayos <datracka@gmail.com>
2024-12-26 12:14:08 +0100

    add more logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ async function fetchDatabaseStatus() {
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error("Database connection error:", error.message);
-      dbMessage = `Failed to connect to the database: ${error.message} ${process.env.DATABASE_HOST} ${process.env.DATABASE_PORT} ${process.env.DATABASE_USER} ${process.env.DATABASE_PASSWORD} ${process.env.DATABASE_NAME}`;
+      dbMessage = `Failed to connect to the database: ${error.message} DATABASE_HOST: ${process.env.DATABASE_HOST} DATABASE_PORT: ${process.env.DATABASE_PORT} DATABASE_USER: ${process.env.DATABASE_USER} DATABASE_PASSWORD: ${process.env.DATABASE_PASSWORD} DATABASE_NAME: ${process.env.DATABASE_NAME}`;
     } else {
       console.error("Unexpected error:", error);
       dbMessage = "Failed to connect to the database: An unexpected error occurred.";
